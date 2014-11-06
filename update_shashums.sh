aa2618458fd678d33cf5ea95c7c5e5eb6c15332c
Dane Springmeyer <dane@mapbox.com>
2014-11-05 19:25:14 -0500

    try updating shasums

---
+++ update_shashums.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRATCH_DIR=/tmp/${NODE_VERSION}
+aws s3 cp --recursive ${S3_URL}/${NODE_VERSION} ${SCRATCH_DIR}
+cd ${SCRATCH_DIR}
+for i in $(ls ./*.*); do 
+  shasum $i >> ./SHASUMS.txt
+  shasum -a 256 $i >> ./SHASUMS256.txt
+done
+for i in $(ls ./x64/*.*); do 
+  shasum $i >> ./SHASUMS.txt
+  shasum -a 256 $i >> ./SHASUMS256.txt
+done
+
+aws s3 cp --acl public-read ./SHASUMS.txt ${S3_URL}/${NODE_VERSION}/
+aws s3 cp --acl public-read ./SHASUMS256.txt ${S3_URL}/${NODE_VERSION}/

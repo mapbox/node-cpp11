6cf13181b34b5437c0e83fcbdd21145de7374c9a
Dane Springmeyer <dane@mapbox.com>
2014-11-05 20:46:15 -0500

    fix up shasum update script

--- update_shashums.sh
+++ update_shashums.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-SCRATCH_DIR=/tmp/${NODE_VERSION}
+if [[ ${NODE_VERSION:-false} == false ]]; then
+    echo '${NODE_VERSION}' must be defined
+    exit 1
+fi
+
+if [[ ${S3_URL}:-false} == false ]]; then
+    echo '${S3_URL}' must be defined
+    exit 1
+fi
+
+SCRATCH_DIR=/tmp/v${NODE_VERSION}
 mkdir -p ${SCRATCH_DIR}
-aws s3 cp --recursive ${S3_URL}/${NODE_VERSION} ${SCRATCH_DIR}
+aws s3 cp --recursive ${S3_URL}/v${NODE_VERSION}/ ${SCRATCH_DIR}/
 cd ${SCRATCH_DIR}
 for i in $(ls ./*.*); do 
   shasum $i >> ./SHASUMS.txt
@@ -13,5 +23,5 @@ for i in $(ls ./x64/*.*); do
   shasum -a 256 $i >> ./SHASUMS256.txt
 done
 
-aws s3 cp --acl public-read ./SHASUMS.txt ${S3_URL}/${NODE_VERSION}/
-aws s3 cp --acl public-read ./SHASUMS256.txt ${S3_URL}/${NODE_VERSION}/
+aws s3 cp --acl public-read ./SHASUMS.txt ${S3_URL}/v${NODE_VERSION}/
+aws s3 cp --acl public-read ./SHASUMS256.txt ${S3_URL}/v${NODE_VERSION}/

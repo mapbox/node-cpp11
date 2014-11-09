c380233e139b59b2fdab630c3dfdc0f69a493049
Young Hahn <young@mapbox.com>
2014-11-08 22:31:17 -0500

    More explicit naming of aws creds of two accounts

---
+++ .travis.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# launches cfnci stack in sandbox
+# passes BUILD_* keys to cfnci stack for publishing to mapbox bucket
+AWS_ACCESS_KEY_ID=$SANDBOX_AWS_ACCESS_KEY_ID \
+AWS_SECRET_ACCESS_KEY=$SANDBOX_AWS_SECRET_ACCESS_KEY \
+BUILD_AWS_ACCESS_KEY_ID=$MAPBOX_AWS_ACCESS_KEY_ID \
+BUILD_AWS_SECRET_ACCESS_KEY=$MAPBOX_AWS_SECRET_ACCESS_KEY \
+./cfn_build_node.sh
+
+# uploads shasums to mapbox bucket
+AWS_ACCESS_KEY_ID=$MAPBOX_AWS_ACCESS_KEY_ID \
+AWS_SECRET_ACCESS_KEY=$MAPBOX_AWS_SECRET_ACCESS_KEY \
+- ./update_shashums.sh

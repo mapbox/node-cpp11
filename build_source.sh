13a01e688f47dbc45b416a2fe4b221d33c62075e
Young Hahn <young@mapbox.com>
2014-11-10 12:12:11 -0500

    Typo

--- build_source.sh
+++ build_source.sh
@@ -13,7 +13,7 @@ fi
 set -e -u
 
 BUILD_DIR=/tmp/v${NODE_VERSION}
-git clone https://github.com/mapbox/node.git -b v%NODE_VERSION%-nodecpp11 $BUILD_DIR
+git clone https://github.com/mapbox/node.git -b v${NODE_VERSION}-nodecpp11 $BUILD_DIR
 ./configure --prefix=$BUILD_DIR
 make node-v${NODE_VERSION}.tar.gz
 aws s3 cp --acl=public-read node-v${NODE_VERSION}.tar.gz ${S3_URL}/v${NODE_VERSION}/node-v${NODE_VERSION}.tar.gz

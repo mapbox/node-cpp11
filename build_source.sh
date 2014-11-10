3dae6db4571206277d90fe8495d58c657b86d780
Young Hahn <young@mapbox.com>
2014-11-10 12:52:57 -0500

    cd to the node dir

--- build_source.sh
+++ build_source.sh
@@ -12,9 +12,13 @@ fi
 
 set -e -u
 
+CWD=$(pwd)
 BUILD_DIR=/tmp/v${NODE_VERSION}
+
 git clone https://github.com/mapbox/node.git -b v${NODE_VERSION}-nodecpp11 $BUILD_DIR
+cd $BUILD_DIR
 ./configure --prefix=$BUILD_DIR
 make node-v${NODE_VERSION}.tar.gz
 aws s3 cp --acl=public-read node-v${NODE_VERSION}.tar.gz ${S3_URL}/v${NODE_VERSION}/node-v${NODE_VERSION}.tar.gz
 
+cd $CWD

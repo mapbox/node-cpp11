cdc8bf4118a6018b14df79c991440eab559d2572
Dane Springmeyer <dane@mapbox.com>
2014-11-18 14:11:39 -0800

    use clang + build node in parallel - refs #15 [publish]

--- build_source.sh
+++ build_source.sh
@@ -21,8 +21,11 @@ BUILD_DIR=/tmp/v${NODE_VERSION}
 
 git clone https://github.com/mapbox/node.git -b ${BRANCH} $BUILD_DIR
 cd $BUILD_DIR
+# on travis this should find /usr/local/clang-3.4/bin/clang++
+export CC=clang
+export CXX=clang++
 ./configure --prefix=$BUILD_DIR
-make node-v${NODE_VERSION}.tar.gz
+make -j${JOBS} node-v${NODE_VERSION}.tar.gz
 aws s3 cp --acl=public-read node-v${NODE_VERSION}.tar.gz ${S3_URL}/v${NODE_VERSION}/node-v${NODE_VERSION}.tar.gz
 
 cd $CWD

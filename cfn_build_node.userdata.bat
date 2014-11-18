7a7f1e016c86979b0a6a10f5650b369bfd53cb28
Young Hahn <young@mapbox.com>
2014-11-18 15:58:39 -0500

    Back to master.

--- cfn_build_node.userdata.bat
+++ cfn_build_node.userdata.bat
@@ -3,6 +3,6 @@ cd /d Z:\
 call cinst git.install
 set PATH=%PATH%;C:\Program Files (x86)\Git\cmd;C:\Program Files (x86)\Git\bin
 setx PATH "%PATH%;C:\Program Files (x86)\Git\cmd;C:\Program Files (x86)\Git\bin" /m
-call git clone https://github.com/mapbox/node-cpp11 -b alt-node Z:\node-cpp11
+call git clone https://github.com/mapbox/node-cpp11 Z:\node-cpp11
 call Z:\node-cpp11\windows\install.bat
 

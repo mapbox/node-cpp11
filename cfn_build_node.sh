5931cbece968abd1fd205ea8b2e5f9075211ec0c
Young Hahn <young@mapbox.com>
2014-11-18 15:39:50 -0500

    Bump up cfn-create timeout [publish]

--- cfn_build_node.sh
+++ cfn_build_node.sh
@@ -45,7 +45,7 @@ npm install https://github.com/mapbox/cfn-ci/tarball/windows
 npm install cfn-config
 
 # create cfn stack for building
-timeout 1200 $TMP/node_modules/.bin/cfn-create -f -r us-east-1 -n "travis-node-cpp11-$TRAVIS_JOB_ID" -t $TMP/node_modules/cfn-ci/cfn-win.template -c $ConfigJSON || echo "cfn-create failed, cleaning up ..." &
+timeout 2400 $TMP/node_modules/.bin/cfn-create -f -r us-east-1 -n "travis-node-cpp11-$TRAVIS_JOB_ID" -t $TMP/node_modules/cfn-ci/cfn-win.template -c $ConfigJSON || echo "cfn-create failed, cleaning up ..." &
 
 # Node builds can take a long time.
 # Output for travis to chew on to avoid 10 min "no output" timeout.

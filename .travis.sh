856d454c37bd603a95518bf109f65d2b180b7d02
Dane Springmeyer <dane@mapbox.com>
2014-11-20 21:27:39 -0800

    first pass at mutltios support + publishing c++11 binaries for osx and linux

--- .travis.sh
+++ .travis.sh
@@ -9,25 +9,43 @@ if ! echo "$COMMIT_MESSAGE" | grep '\[publish' > /dev/null; then
     exit 0
 fi
 
-# launches cfnci stack in sandbox
-# passes BUILD_* keys to cfnci stack for publishing to mapbox bucket
-AWS_ACCESS_KEY_ID=$SANDBOX_AWS_ACCESS_KEY_ID \
-AWS_SECRET_ACCESS_KEY=$SANDBOX_AWS_SECRET_ACCESS_KEY \
-BUILD_AWS_ACCESS_KEY_ID=$MAPBOX_AWS_ACCESS_KEY_ID \
-BUILD_AWS_SECRET_ACCESS_KEY=$MAPBOX_AWS_SECRET_ACCESS_KEY \
-./cfn_build_node.sh
+platform=$(uname -s | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/")
+if [[ ${platform} == 'linux' ]]; then
+    # upgrade libstdc++ to support C++11
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt-get update -qqy
+    sudo apt-get install -qqy libstdc++6 curl unzip python-pip
+    sudo pip install awscli
+else if [[ ${platform} == 'darwin' ]]; then
+    xcrun --sdk macosx --show-sdk-version; fi;
+    sudo easy_install awscli
+fi
 
-# sign node binaries
-AWS_ACCESS_KEY_ID=$MAPBOX_AWS_ACCESS_KEY_ID \
-AWS_SECRET_ACCESS_KEY=$MAPBOX_AWS_SECRET_ACCESS_KEY \
-./sign_node.sh
+
+if [[ ${platform} == 'linux' ]]; then
+    # launches cfnci stack in sandbox
+    # passes BUILD_* keys to cfnci stack for publishing to mapbox bucket
+    AWS_ACCESS_KEY_ID=$SANDBOX_AWS_ACCESS_KEY_ID \
+    AWS_SECRET_ACCESS_KEY=$SANDBOX_AWS_SECRET_ACCESS_KEY \
+    BUILD_AWS_ACCESS_KEY_ID=$MAPBOX_AWS_ACCESS_KEY_ID \
+    BUILD_AWS_SECRET_ACCESS_KEY=$MAPBOX_AWS_SECRET_ACCESS_KEY \
+    ./cfn_build_node.sh
+
+    # sign node binaries
+    AWS_ACCESS_KEY_ID=$MAPBOX_AWS_ACCESS_KEY_ID \
+    AWS_SECRET_ACCESS_KEY=$MAPBOX_AWS_SECRET_ACCESS_KEY \
+    ./sign_node.sh
+
+fi
 
 # build node source tarball and publish
 AWS_ACCESS_KEY_ID=$MAPBOX_AWS_ACCESS_KEY_ID \
 AWS_SECRET_ACCESS_KEY=$MAPBOX_AWS_SECRET_ACCESS_KEY \
 ./build_source.sh
 
-# uploads shasums to mapbox bucket
-AWS_ACCESS_KEY_ID=$MAPBOX_AWS_ACCESS_KEY_ID \
-AWS_SECRET_ACCESS_KEY=$MAPBOX_AWS_SECRET_ACCESS_KEY \
-./update_shashums.sh
+if [[ ${platform} == 'linux' ]]; then
+    # uploads shasums to mapbox bucket
+    AWS_ACCESS_KEY_ID=$MAPBOX_AWS_ACCESS_KEY_ID \
+    AWS_SECRET_ACCESS_KEY=$MAPBOX_AWS_SECRET_ACCESS_KEY \
+    ./update_shashums.sh
+fi

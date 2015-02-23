d9e075f5fbae592716e53ecf25be94baad83414f
Dane Springmeyer <dane@mapbox.com>
2015-02-23 15:49:54 -0800

    cleanup zombie git installer processes - refs #20

--- cfn_build_node.userdata.bat
+++ cfn_build_node.userdata.bat
@@ -1,6 +1,9 @@
 :: start on Z:\ drive
 cd /d Z:\
-call cinst git.install
+call cinst -y git.install
+:: https://github.com/mapbox/node-cpp11/issues/20#issuecomment-75631535
+call TaskKill /IM git.installinstall.exe /F
+call TaskKill /IM git.installinstall.tmp /F
 set PATH=%PATH%;C:\Program Files (x86)\Git\cmd;C:\Program Files (x86)\Git\bin
 setx PATH "%PATH%;C:\Program Files (x86)\Git\cmd;C:\Program Files (x86)\Git\bin" /m
 call git clone https://github.com/mapbox/node-cpp11 Z:\node-cpp11

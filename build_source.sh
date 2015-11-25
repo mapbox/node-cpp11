9cabd65beb2a39d44d8676722ff90ca471677dab
Dane Springmeyer <dane@mapbox.com>
2015-11-24 19:05:56 -0500

    better adaptation to node 4/5 changes [publish]

--- build_source.sh
+++ build_source.sh
@@ -23,15 +23,20 @@ git clone https://github.com/mapbox/node.git -b ${BRANCH} $BUILD_DIR
 cd $BUILD_DIR
 ./configure --prefix=$BUILD_DIR
 export platform=$(uname -s | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/")
+SOURCE_TARBALL=node-v${NODE_VERSION}.tar
+# Node 5 and above has a tarball target that does not expect .gz
+BINARY_TARBALL=node-v${NODE_VERSION}-${platform}-x64.tar
+if [[ ${NODE_VERSION} =~ "0.10" ]] || [[ ${NODE_VERSION} =~ "0.12" ]]; then
+    SOURCE_TARBALL="${SOURCE_TARBALL}.gz"
+    BINARY_TARBALL="${BINARY_TARBALL}.gz"
+fi
+
 if [[ ${platform} == 'linux' ]]; then
-    SOURCE_TARBALL=node-v${NODE_VERSION}.tar.gz
     make -j${JOBS} ${SOURCE_TARBALL}
     aws s3 cp --acl=public-read ${SOURCE_TARBALL} ${S3_URL}/v${NODE_VERSION}/${SOURCE_TARBALL}
 fi
 
-# Node 5 and above has a tarball target that does not expect .gz
-BINARY_TARBALL=node-v${NODE_VERSION}-${platform}-x64.tar
-make -j${JOBS} ${BINARY_TARBALL} || make -j${JOBS} ${BINARY_TARBALL}.gz
+make -j${JOBS} ${BINARY_TARBALL}
 aws s3 cp --acl=public-read ${BINARY_TARBALL} ${S3_URL}/v${NODE_VERSION}/${BINARY_TARBALL}
 
 cd $CWD

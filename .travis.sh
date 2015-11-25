eef4e6a4b24419689d4479423392f731fbd737c1
Dane Springmeyer <dane@mapbox.com>
2015-11-24 21:30:13 -0500

    sudo:false, always build from source

--- .travis.sh
+++ .travis.sh
@@ -4,25 +4,9 @@ set -e
 
 export COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 
-if ! echo "$COMMIT_MESSAGE" | grep '\[publish' > /dev/null; then
-    echo "Include [publish] in commit message to build."
-    exit 0
-fi
+pip install --user awscli
 
-export platform=$(uname -s | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/")
-
-if [[ ${platform} == 'linux' ]]; then
-    # upgrade libstdc++ to support C++11
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-    sudo apt-get update -qqy
-    sudo apt-get install -qqy libstdc++6 curl unzip python-pip
-else if [[ ${platform} == 'darwin' ]]; then
-    xcrun --sdk macosx --show-sdk-version; fi;
-fi
-
-sudo pip install awscli
-
-if [[ ${platform} == 'linux' ]]; then
+if [[ ${COMMIT_MESSAGE} =~ "[publish]" ]] && [[ $(uname -s) == 'Linux' ]]; then
     # launches cfnci stack in sandbox
     # passes BUILD_* keys to cfnci stack for publishing to mapbox bucket
     AWS_ACCESS_KEY_ID=$SANDBOX_AWS_ACCESS_KEY_ID \
@@ -42,7 +26,7 @@ AWS_ACCESS_KEY_ID=$MAPBOX_AWS_ACCESS_KEY_ID \
 AWS_SECRET_ACCESS_KEY=$MAPBOX_AWS_SECRET_ACCESS_KEY \
 ./build_source.sh
 
-if [[ ${platform} == 'linux' ]]; then
+if [[ $(uname -s) == 'Linux' ]] && [[ ${COMMIT_MESSAGE} =~ "[publish]" ]]; then
     # uploads shasums to mapbox bucket
     AWS_ACCESS_KEY_ID=$MAPBOX_AWS_ACCESS_KEY_ID \
     AWS_SECRET_ACCESS_KEY=$MAPBOX_AWS_SECRET_ACCESS_KEY \
